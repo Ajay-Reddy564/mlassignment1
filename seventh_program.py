889371224ae185db38479065c24371990625f826
Ajay-Reddy564 <reddyajay1999@gmail.com>
2022-09-01 22:05:28 -0500

    Seventh Program

---
+++ seventh_program.py
@@ -0,0 +1,5 @@
+# Use a tab escape sequence to get the following lines.
+# Name Age Country City
+# Asabeneh 250 Finland Helsinki
+
+print("Name\tAge\t\tCountry\t\tCity\nAjay  \t25\t    India\t\tCuddapah")
