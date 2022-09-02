1cb80103c1007a7ace0364390388432b8711e44c
Ajay-Reddy564 <reddyajay1999@gmail.com>
2022-09-01 22:06:13 -0500

    Eighth Program

---
+++ eighth_program.py
@@ -0,0 +1,10 @@
+# Use the string formatting method to display the following:
+# radius = 10
+# area = 3.14 * radius ** 2
+# “The area of a circle with radius 10 is 314 meters square.”
+
+#given radius
+radius = 10
+area = 3.14 * radius ** 2 #calculating the area of the circle
+
+print(f"The area of a circle with radius {radius} is {area} meters square.") #prining the area of circle with given radius 
